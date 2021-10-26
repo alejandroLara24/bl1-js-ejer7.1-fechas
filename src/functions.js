97fe3047d94260b3f72c6711dcb720137a3eebbe
AlejandroLaraMoncho <alejandrolaram243@gmail.com>
2021-10-26 17:45:12 +0200

    Ejercicio7

--- src/functions.js
+++ src/functions.js
@@ -1,1 +1,40 @@
 'use strict'
+
+function age(birthdate) {
+    let fechaHoy = new Date()
+    var edad = fechaHoy.getFullYear() - birthdate.getFullYear();
+    var meses = fechaHoy.getMonth() - birthdate.getMonth();
+
+    if (meses < 0 || (meses === 0 && fechaHoy.getDate() < birthdate.getDate())) {
+        edad--;
+    }
+    return parseInt(edad)
+}
+
+function dayName(birthdate) {
+    const dias = ["Domingo","Lunes","Martes","Mirécoles","Jueves","Viernes","Sábado"]
+    return dias[birthdate.getDay()]
+}
+
+function birthdateOnYear(birthdate,year) {
+    let birthdateOnYear = new Date(birthdate)
+    birthdateOnYear.setFullYear(year)
+    return birthdateOnYear
+}
+
+function nextBirthdate(birthdate) {
+    let nextBirthdate = new Date(birthdate)
+    return nextBirthdate.setFullYear(nextBirthdate.getFullYear()+1)
+}
+
+function yearsWithBirthdateOnDay(birthdate,maxYear,dayOfWeek) {
+    
+}
+
+module.exports = {
+    age,
+    dayName,
+    birthdateOnYear,
+    nextBirthdate,
+    yearsWithBirthdateOnDay
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 'use strict'
+
+let birthdate
+
+window.addEventListener('load', () => {
+    document.getElementById('formDate').addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        birthdate = new Date(document.getElementById('birthdate').value)
+        let anyos = age(birthdate)
+        let diaTexto = dayName(birthdate)
+        let fechaEnAnyoDistinto = birthdateOnYear(birthdate,2030)
+        let proximoCumpleanyos = nextBirthdate(birthdate)
+
+    })
+})
